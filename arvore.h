733d591f13ee198408d314758f159966c028b5c1
GeorgeRV <georgebada@hotmail.com>
2023-06-27 06:47:10 -0300

    Atualize o menu, implemente as funções para a altura e o tamanho da árvore.

--- arvore.h
+++ arvore.h
@@ -18,12 +18,11 @@ void traversePreOrder(struct Node*);
 void traverseInOrder(struct Node* );
 void traversePosOrder(struct Node*);
 
+int alturaArvore(struct Node* , int iAltura = 0);
+int tamanhoArvore(struct Node* root,  int iTamanho = 0);
 struct Node* insertNode(struct Node* node, int iData);
 struct Node* deleteNode(struct Node* node, int iData);
 struct Node* searchNode(struct Node* node, int iData);
-
-
-
 
 
 struct Node* newNode(int iData){
@@ -128,5 +127,52 @@ struct Node* deleteNode(struct Node* node, int iData)
     }
 }
 
+
+/**
+ * Descobre a altura da árvore.
+ *
+ * Calcula a profundidade máxima de uma árvore binaria, passando por
+ * toda ramificação da árvore, retornando a profundidade da folha
+ * mais profunda.
+ *
+ * @param root Ponterio do Node raiz da árvore.
+ * @param iAltura Inteiro, altura da raiz, inicializado em 0
+ * @return inteiro representado a profundidade da folha mais profunda.
+ */
+int alturaArvore(struct Node* root, int iAltura)
+{
+    if (root != nullptr)
+    {
+        int AltLeft = alturaArvore(root->ptrLeft, iAltura + 1);
+        int AltRight = alturaArvore(root->ptrRight, iAltura + 1);
+
+        return max(AltLeft, AltRight);
+
+    }
+    else return iAltura;
+}
+
+
+/**
+ * Descobre o tamanho da árvore,
+ *
+ * Calcula o tamanho de uma árvore binaria, contado o número de
+ * nós em toda sua ramificação
+ *
+ * @param root Ponterio do Node raiz da árvore
+ * @param iTamanho Inteiro, tamanho da raiz, inicializado em 0
+ * @return inteiro representado o tamanho da árvore.
+ */
+int tamanhoArvore(struct Node* root, int iTamanho)
+{
+    if (root != nullptr)
+    {
+        int iTamLeft = tamanhoArvore(root->ptrLeft, iTamanho + 1);
+        int iTamRight = tamanhoArvore(root->ptrRight, iTamanho + 1);
+
+        return 1 + iTamLeft + iTamRight;
+    }
+    else return 0;
+}
 
 #endif //TRABALHO_DE_ED_ARVORE_H
--- main.cpp
+++ main.cpp
@@ -29,20 +29,20 @@ void printMenu()
             "04. Verificar o tamanho da árvore\n"
             "05. Inserir um elemento\n"
             "06. Remover um elemento\n"
-            "06. Buscar o endereço de memória de um elemento\n"
-            "07. Verificar se a árvore é completa\n"
-            "08. Verificar se a árvore é perfeita\n"
-            "09. Exibir a árvore utilizando (BFS)\n"
-            "10. Converter a árvore em uma lista e ordenar com Bubble Sort\n"
-            "11. Converter a árvore em uma lista e ordenar com Selection Sort\n"
-            "12. Converter a árvore em uma lista e ordenar com Insertion Sort\n"
-            "13. Converter a árvore em uma lista e ordenar com Shell Sort" << endl;
+            "07. Buscar o endereço de memória de um elemento\n"
+            "08. Verificar se a árvore é completa\n"
+            "09. Verificar se a árvore é perfeita\n"
+            "10. Exibir a árvore utilizando (BFS)\n"
+            "11. Converter a árvore em uma lista e ordenar com Bubble Sort\n"
+            "12. Converter a árvore em uma lista e ordenar com Selection Sort\n"
+            "13. Converter a árvore em uma lista e ordenar com Insertion Sort\n"
+            "14. Converter a árvore em uma lista e ordenar com Shell Sort" << endl;
     cout << "Digite: ";
 }
 
-void teste()
+struct Node* teste(struct Node* ptrRoot)
 {
-    struct Node *root = nullptr;
+    struct Node *root = ptrRoot;
 
     for (int i = 0; i < 10; i++) {
         int j = 1 - 2 * (i % 2);
@@ -56,10 +56,16 @@ void teste()
     cout << alturaArvore(root) << endl;
 
     cout << "Tamanho arvore: " << tamanhoArvore(root) << endl;
+
+    return  root;
 }
 
 void menu(int iOpcao) {
+
     int iOpcaos = iOpcao;
+    struct Node* root = nullptr;
+    int iValor;
+
     while (iOpcaos != -1){
 
         system("cls");
@@ -69,14 +75,74 @@ void menu(int iOpcao) {
 
         switch (iOpcaos) {
             case 1:
+                cout << "01. Construir uma árvore binária de busca a partir de um arquivo texto\n" << endl;
+                //root = arvoreTexto();
                 break;
+
+            case 2:
+                cout << "02. Construir uma árvore binária de busca a partir de dados digitados pelo usuário\n" << endl;
+                //root = arvoreUsuario();
+                break;
+
+            case 3:
+                cout << "03. Verificar a altura da árvore\n" << endl;
+                cout << "Altura arvore: " << alturaArvore(root) << endl;
+                break;
+
+            case 4:
+                cout << "04. Verificar o tamanho da árvore\n" << endl;
+                cout << "Tamanho arvore: " << tamanhoArvore(root) << endl;
+                break;
+
+            case 5:
+                cout << "05. Inserir um elemento\n" << endl;
+                cout << "Digite um elemento: ";
+                cin >> iValor;
+
+                insertNode(root, iValor);
+                break;
+
+            case 6:
+                cout << "06. Remover um elemento\n" << endl;
+                break;
+
+            case 7:
+                cout << "07. Buscar o endereço de memória de um elemento\n" << endl;
+                break;
+
+            case 8:
+                cout << "08. Verificar se a árvore é completa\n" << endl;
+                break;
+
+            case 9:
+                cout << "09. Verificar se a árvore é perfeita\n" << endl;
+                break;
+
+            case 10:
+                cout << "10. Exibir a árvore utilizando (BFS)\n" << endl;
+                break;
+
+            case 11:
+                cout << "11. Converter a árvore em uma lista e ordenar com Bubble Sort\n" << endl;
+                break;
+
+            case 12:
+                cout << "12. Converter a árvore em uma lista e ordenar com Selection Sort\n" << endl;
+                break;
+
+            case 13:
+                cout << "13. Converter a árvore em uma lista e ordenar com Insertion Sort\n" << endl;
+                break;
+
+            case 14:
+                cout << "14. Converter a árvore em uma lista e ordenar com Shell Sort\n" << endl;
 
             case -1:
                 cout << "Tchau!" << endl;
                 break;
 
             default:
-                teste();
+                root = teste(root);
                 break;
 
         }
